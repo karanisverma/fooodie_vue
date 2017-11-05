42df68d91f28350c780d3088e9ff2ad14452cdbc
karan <kverma@encyclokidia.com>
2017-11-05 17:48:24 +0530

    init commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+  {{ $store.state.count }}
+
+  {{evenOrOdd}}
+  {{yOrn}}
+    <img src="./assets/logo.png">
+    <Menu></Menu>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import Menu from './components/Menu'
+export default {
+  name: 'app',
+  components: {
+    Menu
+  },
+  computed: mapGetters([
+    'evenOrOdd',
+    'yOrn'
+  ])
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
