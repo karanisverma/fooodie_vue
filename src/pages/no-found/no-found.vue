fe6d41fc5a0918a8d41ffe1253381d06088eb509
suprabha <suprabha@zoomcar.com>
2017-11-22 14:35:08 +0530

    fix(no-found-css): changed css for no-found page

--- src/pages/no-found/no-found.vue
+++ src/pages/no-found/no-found.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="page-not-found">
-    Page Not Found
+  	<div class="not-found">
+	    <p class="error">Oops Error!</p>
+	    <p class="status">404</p>
+	    <p class="error-msg">Sorry, the page not found.</p>
+	    <a href="/">Go To Homepage</a>
+	</div>
   </div>
 </template>
 
