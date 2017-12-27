c53fdd91ace153b77a3d9e8e333e4b492af15207
suprabha <suprabha@zoomcar.com>
2017-12-27 14:35:06 +0530

    feat(testimonial): added testimonial

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -60,9 +60,40 @@
     </div>
 
     <!-- Testimonial -->
+    <div class="web-wrapper web-view">
+      <div class="testimonial">
+        <input type="radio" name="image-slider" class="to-one" checked>
+        <input type="radio" name="image-slider" class="to-two">
+        <input type="radio" name="image-slider" class="to-three">
+        <input type="radio" name="image-slider" class="to-four">
+        <div class="images">
+          <div class="slide-one">
+            <img src="../../assets/reviewer/akanksha.jpg">
+            <p>It's good to have such kind of service in our home town, delicious food, punctual delivery, thanks foodie, even my parents liked the food. Keep growing .</p>
+            <p>- Akansha Shrivastava</p>
+            <p>Student</p>
+          </div>
+          <div class="slide-two">
+            <img src="../../assets/reviewer/Paromita_Patra.jpg">
+            <p>Getting this kind of doorstep service in our little town is such a delight.. The food quality is as good as we get in any other restaurant in town.. The best part is their punctual delivery.. I hope you guys keep up your standards.. All the best</p>
+            <p>Dr.Paromita Patra</p>
+            <p>Doctor</p>
+          </div>
+          <div class="slide-three">
+            <img src="../../assets/reviewer/rahul_pandey.jpg">
+            <p>Good job guys..the food is good and the rate is great too. Wish u success ahead!!</p>
+            <p>Rahul Pandey</p>
+            <p>Engineer</p>
+          </div>
+          <div class="slide-four">
+            <img src="../../assets/reviewer/akanksha_kashyap.jpg">
+            <p>Amazing food again...your making me addicted towards you guys... love the cooking.... God bless you...</p>
+            <p>Akanksha Kashyap</p>
+            <p>Professor</p>
+          </div>
 
-    <div class="testimonial">
-      <p>testimonial will come here</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
