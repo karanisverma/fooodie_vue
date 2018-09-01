7ac362641f6185786cbd370446c6914dd9375639
karan <kverma@encyclokidia.com>
2018-09-02 00:23:32 +0530

    fix: removed console logs

--- src/store/modules/products.js
+++ src/store/modules/products.js
@@ -7,14 +7,12 @@ class ProductState {
     this.allProducts = {}
     this.quantity = 0
     this.APIHOST = 'https://api.fooodie.store'
-    // this.APIHOST = 'http://34.154.40.23'
+    // this.APIHOST = 'http://35.154.40.23'
     this.header = { 'Content-Type': 'application/json' }
   }
   getAllProduct () {
-    // https://fooodie.store/static/js/category.json
     return new Promise((resolve, reject) => {
-      // fetch(this.APIHOST + '/assets/js/products.json', {
-      fetch(this.APIHOST + '/api/v1/products', {
+      fetch(this.APIHOST + '/api/v1/products/', {
         method: 'GET'
       })
         .then(res => res.json())
@@ -22,7 +20,6 @@ class ProductState {
           let fullProductItem = response.objects.map(product =>
             Object.assign({}, product, { quantity: 0 })
           )
-          // console.log('fullProductItem====>', fullProductItem)
           resolve(fullProductItem)
         })
         .catch(error => {
